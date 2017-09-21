f46e8bff06873e4d3079358ecac87d2982eb7640
saanamarika <saana.m.sieppi@gmail.com>
2017-09-21 15:45:40 +0300

    Initial project

---
+++ condiniotal-statements/conditional-stament/conditional-stament/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace conditional_stament
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma tulostaa positiivinen, negatiivinen tai nolla");
+            Console.Write("Syötä luku: ");
+            string userInput;
+            userInput = Console.ReadLine();
+        
+            int.TryParse(userInput, out int evaluatedNumber);
+
+            if (evaluatedNumber < 0)
+            {
+                Console.WriteLine($"Numero {evaluatedNumber} on negatiivinen");
+            }
+            else if (evaluatedNumber > 0)
+            {
+                Console.WriteLine($"Numero {evaluatedNumber} on posiitivinen");
+            }
+            else
+            {
+                Console.WriteLine($"Numero {evaluatedNumber} on nolla");
+            }
+            
+           
+            Console.ReadKey();
+
+        }
+    }
+}
