c8049b1cffb3b216a37d50d1693e3c459edeca9e
saanamarika <saana.m.sieppi@gmail.com>
2017-09-22 13:02:47 +0300

    Looptest

--- condiniotal-statements/conditional-stament/conditional-stament/Program.cs
+++ condiniotal-statements/conditional-stament/conditional-stament/Program.cs
@@ -6,27 +6,40 @@ namespace conditional_stament
     {
         static void Main(string[] args)
         {
+            // define instructions
             Console.WriteLine("Ohjelma tulostaa positiivinen, negatiivinen tai nolla");
-            Console.Write("Syötä luku: ");
-            string userInput;
-            userInput = Console.ReadLine();
-        
-            int.TryParse(userInput, out int evaluatedNumber);
+            bool isNumber;
+            do
+            {
+                Console.Write("Syota luku: ");
+                // define variables
+                string userInput;
+                userInput = Console.ReadLine();
 
-            if (evaluatedNumber < 0)
-            {
-                Console.WriteLine($"Numero {evaluatedNumber} on negatiivinen");
-            }
-            else if (evaluatedNumber > 0)
-            {
-                Console.WriteLine($"Numero {evaluatedNumber} on posiitivinen");
-            }
-            else
-            {
-                Console.WriteLine($"Numero {evaluatedNumber} on nolla");
-            }
-            
-           
+                int evaluatedNumber;
+                isNumber = int.TryParse(userInput, out evaluatedNumber);
+
+                // program logic
+                if (isNumber == true)
+                {
+                    if (evaluatedNumber < 0)
+                    {
+                        Console.WriteLine($"Numero {evaluatedNumber} on negatiivinen");
+                    }
+                    else if (evaluatedNumber > 0)
+                    {
+                        Console.WriteLine($"Numero {evaluatedNumber} on posiitivinen");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Numero {evaluatedNumber} on nolla");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Syotit muuta kuin numeroita!");
+                }
+            } while (isNumber == false);
             Console.ReadKey();
 
         }
