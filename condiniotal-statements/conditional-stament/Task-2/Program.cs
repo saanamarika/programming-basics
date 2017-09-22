2a713bafda3ea562db3d4a8a7c50e0b96bcce1bf
saanamarika <saana.m.sieppi@gmail.com>
2017-09-22 15:03:13 +0300

    Task-3

---
+++ condiniotal-statements/conditional-stament/Task-2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // define instructions
+            Console.WriteLine("Ohjelma selvittaa onko syotetty luku pariton vai parillinen.");
+            bool isNumber;
+            Console.Write("Syota luku: ");
+                // define variables
+                string userInput;
+                userInput = Console.ReadLine();
+
+                int evaluatedNumber;
+                isNumber = int.TryParse(userInput, out evaluatedNumber);
+
+                // program logic
+                if(evaluatedNumber % 2 == 0)
+            {
+                Console.WriteLine($"Syotit luvun {evaluatedNumber}, se on parillinen");
+            }
+                else
+            {
+                Console.WriteLine($"Syotit luvun {evaluatedNumber}, se on pariton");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ condiniotal-statements/conditional-stament/Task-3/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            // define instructions
+            Console.WriteLine("Ohjelma tulostaa positiivinen, negatiivinen tai nolla");
+            bool isNumber;
+            do
+            {
+                Console.Write("Syötä luku: ");
+                // define variables
+                string userInput;
+                userInput = Console.ReadLine();
+
+                int evaluatedNumber;
+                isNumber = int.TryParse(userInput, out evaluatedNumber);
+
+
+                // program logic
+                if (isNumber == true)
+                {
+                    if (evaluatedNumber < 0)
+                    {
+                        if (evaluatedNumber % 2 == 0)
+                        {
+                            Console.WriteLine($"Numero {evaluatedNumber} on negatiivinen ja parillinen");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Numero {evaluatedNumber} on negatiivinen ja pariton");
+                        }
+                    }
+                    else if (evaluatedNumber > 0)
+                    {
+                        if (evaluatedNumber % 2 == 0)
+                        {
+                            Console.WriteLine($"Numero {evaluatedNumber} on posiitivinen ja parillinen");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Numero {evaluatedNumber} on positiivinen ja pariton");
+                        }
+                    }
+                    else
+                    {
+                        if (evaluatedNumber % 2 == 0)
+                            Console.WriteLine($"Numero {evaluatedNumber} on nolla ja parillinen");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Syötit muuta kuin numeroita!");
+                }
+            } while (isNumber == false) ;
+                Console.ReadKey();
+
+            
+        }
+    }
+}
+
