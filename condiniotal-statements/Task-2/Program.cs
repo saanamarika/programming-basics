bc0faabccd3790c6f9a58a207e5c4790b56102d6
saanamarika <saana.sieppi@student.saimia.fi>
2017-10-13 10:41:33 +0300

    Loop-2

---
+++ condiniotal-statements/Task-2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Ohjelma kirjoittaa kertotaulun luvuille 1-9");
+            for (int i = 1; i <= 9; i++)
+            {
+                for (int j=1; j<=9; j++)
+                {
+                    Console.WriteLine($"{i}x{j}={i*j}");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ condiniotal-statements/conditional-stament/Loop-2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Loop_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Ohjelma tulostaa kokonaisluvut 1-10 ja niiden neliöjuuret");
+            Console.WriteLine("Luku\t\tNeliöjuuri");
+            for(int i=1; i<=10; i++)
+            {
+                Console.WriteLine($"{i}\t\t{Math.Round(Math.Sqrt(i),2)}");
+                
+            }
+            Console.ReadKey();
+        }
+    }
+}
