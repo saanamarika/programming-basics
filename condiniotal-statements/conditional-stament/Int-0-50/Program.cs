3220722f38cd8181270d9e89c9dac19370dfb902
saanamarika <saana.sieppi@student.saimia.fi>
2017-10-13 11:16:48 +0300

    Int-0-50.csproj

---
+++ condiniotal-statements/conditional-stament/Int-0-50/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Int_0_50
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Ohjelma tulostaa 20 satunnaista kokonaislukua väliltä 0-50. Luvut tulostetaan 5/per rivi");
+            Random rnd = new Random();
+
+            for (int i=1; i<5; i++)
+            {
+                Console.Write($"Rivi{i}: ");
+                for(int j=0; j<5; j++)
+                {
+                    if (j < 4)
+                    {
+                        Console.Write($"{rnd.Next(51)},");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{rnd.Next(51)}");
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
