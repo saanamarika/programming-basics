574e0aace6b95d3acebd0f47d0d4d3239e4ebe47
saanamarika <saana.sieppi@student.saimia.fi>
2017-10-20 10:57:13 +0300

    loop

---
+++ condiniotal-statements/conditional-stament/vakioveikkaus/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace vakioveikkaus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Ohjelma tulostaa vakioveikkauksen oikean rivin");
+            Random rand = new Random();
+            double randNumber = 0.0;
+
+            for (int i = 1; i <= 13; i++)
+            {
+                randNumber = rand.NextDouble();
+                if (randNumber < 0.4)
+                {
+                    Console.WriteLine($"{i}. 1");
+                }
+                else if (randNumber > 0.4 && randNumber < 0.6)
+                {
+                    Console.WriteLine($"{i}. X");
+                }
+                else
+                {
+                    Console.WriteLine($"{i}. 2");
+                }
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
