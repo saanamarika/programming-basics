0de25b4f70b038bd959932869224f7a546147af7
saanamarika <saana.sieppi@student.saimia.fi>
2017-10-20 21:14:24 +0300

    trying to do dice

---
+++ condiniotal-statements/conditional-stament/Task-1000/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task_1000
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Ohjelma simuloin nopan heittoa 1000 kertaa ja tulostaa lisäksi kuutosten esiintymiskertojen lukumäärän");
+            Random rand = new Random();
+            int total;
+
+            for (int i=1; i<=1000; i++)
+            {
+                Console.WriteLine($"{i}. {rand.Next(1,7)}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
