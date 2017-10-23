e6f789b6bbe3fbb1a53a51fcb6405e2ae1f1f00d
saanamarika <saana.sieppi@student.saimia.fi>
2017-10-23 10:15:10 +0300

    f-1

---
+++ condiniotal-statements/conditional-stament/Funktio-2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Funktio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
