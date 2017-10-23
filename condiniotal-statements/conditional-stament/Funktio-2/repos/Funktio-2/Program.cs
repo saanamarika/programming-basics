3c4bf7700e0c2214efeba378e80c3eedd1c65b5d
saanamarika <saana.sieppi@student.saimia.fi>
2017-10-23 11:11:03 +0300

    function 2

--- condiniotal-statements/conditional-stament/Funktio-2/repos/Funktio-2/Program.cs
+++ condiniotal-statements/conditional-stament/Funktio-2/repos/Funktio-2/Program.cs
@@ -8,7 +8,26 @@ namespace Funktio_2
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
             Console.WriteLine("Ohjelma palauttaa arvonaan kahdesta luvusta pienemmän. Luvut välitetään funktiolle parametrinä");
+            Console.Write("Syötä luku 1/2: ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.Write("Syötä luku 2/2: ");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine(Minimi(num1, num2));
+            Console.ReadKey();
 
+
+
+        }
+        static string Minimi(int x, int y)
+        {
+            if (x<y)
+            {
+                return ($"Luku {x} on pienempi kuin {y}");
+            }
+            else
+            {
+                return ($"Luku {y} on pienempi kuin {x}");
+            }
         }
     }
 }
