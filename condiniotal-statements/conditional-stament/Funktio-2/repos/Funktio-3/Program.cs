49a8862d9a9169bf8ea9bad509573d98257853a6
saanamarika <saana.sieppi@student.saimia.fi>
2017-10-23 12:46:55 +0300

    function 3

---
+++ condiniotal-statements/conditional-stament/Funktio-2/repos/Funktio-3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Funktio_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Ohjelma tarkistaa antamasi luvun onko se 1-10 väliltä, pyytää syöttämään uudelleen jos olet hölmö");
+            int retNumber = numberFromRange(-2, 10);
+            Console.WriteLine($"Syötit kelvollisen luvun.");
+            Console.ReadKey();
+           
+        }
+        static int numberFromRange(int lowerBound, int upperBound)
+        {
+            int number = 0;
+            bool numb = false;
+            do
+            {
+                Console.Write($"Syötä luku {lowerBound}-{upperBound} väliltä: ");
+                numb = int.TryParse(Console.ReadLine(), out number);
+                if(!numb)
+                {
+                    Console.WriteLine("Olet hölmö. ");
+                }
+
+            }
+            while (number < lowerBound || number > upperBound||!numb);
+            return number;
+        }
+        
+    }
+}
