aad705bbd11f41298e85f38b6debd32708eedc4b
saanamarika <saana.sieppi@student.saimia.fi>
2017-10-23 10:49:56 +0300

    f-2

---
+++ condiniotal-statements/conditional-stament/Funktio-2/repos/Funktio-2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Funktio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Ohjelma palauttaa arvonaan kahdesta luvusta pienemmän. Luvut välitetään funktiolle parametrinä");
+
+        }
+    }
+}
---
+++ condiniotal-statements/conditional-stament/Funktio-2/repos/funktio-1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace funktio_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Ohjelma antaa saman verran tähtiä kuin käyttäjän antama numero.");
+            int number = 0;
+            do
+            {
+                Console.Write("Syötä luku: ");
+                number = int.Parse(Console.ReadLine());
+                if (number < 0)
+                {
+                    Console.WriteLine("Väärä syöte!");
+                }
+                else
+                {
+                    Console.WriteLine(Stars(number));
+                }
+            }
+            while (number<0);
+            Console.ReadKey();
+
+
+        }
+
+        static string Stars(int numberOfStars)
+        {
+            string s = "";
+            for (int i = 0; i < numberOfStars; i++)
+            {
+                s += "*";
+            }
+            return s;
+
+        }
+
+    }
+}
