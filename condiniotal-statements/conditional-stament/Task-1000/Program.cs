2d06ab2f2aaa7fd94c40dbafd2f78649f48b43ff
saanamarika <saana.sieppi@student.saimia.fi>
2017-10-23 09:21:48 +0300

    task-1000 ready

--- condiniotal-statements/conditional-stament/Task-1000/Program.cs
+++ condiniotal-statements/conditional-stament/Task-1000/Program.cs
@@ -9,10 +9,18 @@ namespace Task_1000
             Console.OutputEncoding = System.Text.Encoding.UTF8;
             Console.WriteLine("Ohjelma simuloin nopan heittoa 1000 kertaa ja tulostaa lisäksi kuutosten esiintymiskertojen lukumäärän");
             Random rand = new Random();
+            int x = 0;
+            int kuusi = 0;
             for (int i=1; i<=1000; i++)
             {
-                Console.WriteLine($"{i}. {rand.Next(1,7)}");
+                x = rand.Next(1, 7);
+                Console.WriteLine($"{i}. {x}");
+                if (x == 6)
+                {
+                    kuusi++;
+                }
             }
+            Console.WriteLine($"Kuutonen arvottiin {kuusi} kertaa.");
             Console.ReadKey();
         }
     }
