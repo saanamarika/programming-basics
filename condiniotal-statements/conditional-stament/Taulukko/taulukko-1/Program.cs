4efcd3519aec0034012b7234e295b8d65bed5d39
saanamarika <saana.sieppi@student.saimia.fi>
2017-10-24 13:25:40 +0300

    table

---
+++ condiniotal-statements/conditional-stament/Taulukko/taulukko-1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace taulukko_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Ohjelma arpoo 100 lukua 0-50 väliltä ja laskee niiden keskiarvon.");
+            int[] iT = new int[100];
+            double sum = 0;
+            double avg = 0;
+            Random rnd = new Random();
+            for(int i=0; i<iT.Length; i++)
+            {
+                iT[i]=rnd.Next(51);
+            }
+            for (int i=0; i<iT.Length;i++)
+            {
+                sum += iT[i];
+            }
+            avg = sum / iT.Length;
+            for(int i = 0; i < iT.Length; i++)
+            {
+                Console.WriteLine($"{i+1}.\t{iT[i]}");
+            }
+
+            Console.WriteLine($"\nKa.= {avg}");
+
+            Console.ReadKey();
+        }
+    }
+}
