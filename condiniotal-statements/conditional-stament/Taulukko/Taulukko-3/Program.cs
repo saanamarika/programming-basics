efc546ebe709f0af3a8a99c3f6cbb07d2c4cb5a4
saanamarika <saana.sieppi@student.saimia.fi>
2017-10-25 09:37:47 +0300

    table-3

---
+++ condiniotal-statements/conditional-stament/Taulukko/Taulukko-3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Taulukko_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Ohjelma arpoo yksiulotteiseen taulukkoon kokonaislukuja. Taulukon arvot 0-20");
+            Random rnd = new Random();
+           
+            int[] one = new int[10];
+            Console.WriteLine("[x]\tArvo");
+            for(int i=0; i<one.Length;i++)
+            {
+                one[i] = rnd.Next(20);
+                Console.WriteLine($"[{i}]\t{one[i]:00}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
