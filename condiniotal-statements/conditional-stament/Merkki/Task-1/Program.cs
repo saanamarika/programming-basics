aeba17bdfca49baf7605c641f8b315b43f5fa4e1
saanamarika <saana.sieppi@student.saimia.fi>
2017-11-02 12:30:27 +0200

    markline-1

---
+++ condiniotal-statements/conditional-stament/Merkki/Task-1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            string jono = "hello world!";
+            Console.WriteLine(jono);
+            Console.WriteLine($"Syötteessä on {jono.Length} merkkiä.");
+            Console.ReadKey();
+        }
+    }
+}
