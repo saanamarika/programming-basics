c5b090923359003b8aaea1767deab2dc48d2ee8c
saanamarika <saana.sieppi@student.saimia.fi>
2017-11-02 13:18:10 +0200

    mark-3

---
+++ condiniotal-statements/conditional-stament/Merkki/Task-2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            string line = "Vesihiisi sihisi hississä";
+            Console.WriteLine(line);
+
+            line=line.Replace('s', '@');
+            Console.WriteLine(line);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ condiniotal-statements/conditional-stament/Merkki/Task-3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.ReadKey();
+        }
+    }
+}
