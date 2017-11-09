06df480ab57268f7013f28bef07b00c2c9614810
saanamarika <saana.sieppi@student.saimia.fi>
2017-11-09 09:03:58 +0200

    kertaus2

---
+++ condiniotal-statements/Kertaus2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Kertaus2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.Write("Syötä sana tai lause: ");
+            string useri = Console.ReadLine();
+            for (int i = 1; i < useri.Length+1; i++)
+            {
+                Console.WriteLine($"{i}. {useri}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
