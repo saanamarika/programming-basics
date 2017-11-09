1b6d9c8472f3bb33eb27db4ebe9f5a5a11c1dc8b
saanamarika <saana.sieppi@student.saimia.fi>
2017-11-09 09:11:09 +0200

    moving

---
+++ condiniotal-statements/conditional-stament/Kertaus/Kertaus3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Kertaus3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Ohjelma pyytää syöttämään lukuja kunnes syötät -1, jonka jälkeen ohjelma laskee lukujen yhteissumman.");
+            int num = 0;
+            int sum = 0;
+            do
+            {
+                sum += num;
+                Console.Write("Syötä luku: ");
+                num = int.Parse(Console.ReadLine());
+                
+            }
+            while (num > 0 || num < -2);
+            Console.WriteLine($"Lukujen summa on: {sum}");
+            Console.ReadKey();
+            
+        }
+    }
+}
