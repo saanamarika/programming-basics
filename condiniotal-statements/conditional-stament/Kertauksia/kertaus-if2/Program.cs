6851ba726bd06d0845a61eaee50265a9502af1b0
saanamarika <saana.sieppi@student.saimia.fi>
2017-11-10 09:10:20 +0200

    if-again

---
+++ condiniotal-statements/conditional-stament/Kertauksia/kertaus-if2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace kertaus_if2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Ohjelma pyytää syöttämään luvun ja kertoo sitten, onko luku pariton vai parillinen");
+            Console.Write("Syötä luku: ");
+
+        }
+    }
+}
