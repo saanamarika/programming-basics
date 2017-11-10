2e4924a6a412ae7846f4e9a4f0a8c0ade3a88266
saanamarika <saana.sieppi@student.saimia.fi>
2017-11-10 10:00:24 +0200

    table2

---
+++ condiniotal-statements/conditional-stament/Kertaus4/Kertaustaulukko/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Kertaustaulukko
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            int[] number = new int[5];
+            Random rnd = new Random();
+            int randomIndex = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                randomIndex = rnd.Next(11);
+                Console.WriteLine($"{randomIndex} {Stars(randomIndex)}");
+            }
+            Console.ReadKey();
+        }
+        static string Stars(int numberOfStars)
+        {
+            string s = "";
+            for (int i = 0; i < numberOfStars; i++)
+            {
+                s += "*";
+            }
+            return s;
+        }
+    }
+}
