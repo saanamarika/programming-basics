fc1fa7404359e72ff036e430001993d6cdab2eb7
saanamarika <saana.sieppi@student.saimia.fi>
2017-11-10 10:55:04 +0200

    task-4

--- condiniotal-statements/conditional-stament/Merkki/Task-3/Program.cs
+++ condiniotal-statements/conditional-stament/Merkki/Task-3/Program.cs
@@ -7,6 +7,18 @@ namespace Task_3
         static void Main(string[] args)
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.Write("Syötä sana tai lause: ");
+            string input = Console.ReadLine().ToUpper();
+            int chCount = 0;
+            for(int i=0; i<input.Length;i++)
+            {
+                if (input[i]=='A')
+                {
+                    chCount++;
+                }
+
+            }
+            Console.WriteLine($"Syötteessä {input} on {chCount} A-kirjainta");
 
             Console.ReadKey();
         }
---
+++ condiniotal-statements/conditional-stament/Merkki/Task-4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("ohjelma kysyy käyttäjältä sanan tai lauseen ja katsoo onko se palintromi.");
+            Console.Write("Syötä sana tai lause: ");
+
+        }
+    }
+}
