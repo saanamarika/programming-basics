073634d013ffc482524d54853b358ba76a9eeff5
saanamarika <saana.sieppi@student.saimia.fi>
2017-11-30 12:17:47 +0200

    plaa

--- condiniotal-statements/conditional-stament/Merkki/Task-4/Program.cs
+++ condiniotal-statements/conditional-stament/Merkki/Task-4/Program.cs
@@ -9,7 +9,24 @@ namespace Task_4
             Console.OutputEncoding = System.Text.Encoding.UTF8;
             Console.WriteLine("ohjelma kysyy käyttäjältä sanan tai lauseen ja katsoo onko se palintromi.");
             Console.Write("Syötä sana tai lause: ");
+            string palli = Console.ReadLine();
+            string palli2 = palli.Replace(" ", "");
+            string palli3 = "";
+            for(int i=palli2.Length-1; i>=0;i--)
+            {
+                palli3 += palli2[i];
+            }
+            if(palli3==palli2)
+            {
+                Console.WriteLine($"{palli} on palintromi");
+            }
+            else
+            {
+                Console.WriteLine($"{palli} ei ole palintromi");
+            }
+            
 
+            Console.ReadKey();
         }
     }
 }
