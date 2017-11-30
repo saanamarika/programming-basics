d38aa337e92c36431f7c4e89dee1827e688f6751
saanamarika <saana.sieppi@student.saimia.fi>
2017-11-30 10:17:01 +0200

    do it again

---
+++ condiniotal-statements/conditional-stament/Do-it-again/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Do_it_again
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Ohjelma pyytää syöttämään lukuja kunnes syötät -1, jonka jälkeen ohjelma laskee lukujen yhteissumman.");
+            int num = 0;
+            int sum = 0;
+            do
+            {
+                Console.Write("Syötä luku: ");
+                num = int.Parse(Console.ReadLine());
+                sum += num;
+            }
+            while ();
+            Console.WriteLine($"Lukujen summa on: {sum}");
+            Console.ReadKey();
+        }
+    }
+}
