ba7cab9f6726d81dcf563108d74a4cf00b8770b0
saanamarika <saana.sieppi@student.saimia.fi>
2017-11-30 12:59:13 +0200

    vokaali

---
+++ condiniotal-statements/conditional-stament/Vokaali/Vokaali/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Vokaali
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Ohjelma laskee syöttämäsi sanan tai lauseen vokaalien lukumäärän");
+            Console.Write("Syötä data: ");
+            string voka = Console.ReadLine();
+            string voka2 = voka.ToUpperInvariant;
+            string voka7 = voka.Replace(" ", "");
+            int voka3 = voka.Length;
+             voka2 = voka.Replace("A","");
+             voka2 = voka.Replace("E", "");
+             voka2 = voka.Replace("I", "");
+             voka2 = voka.Replace("U", "");
+             voka2 = voka.Replace("Y", "");
+             voka2 = voka.Replace("Ä", "");
+             voka2 = voka.Replace("Ö", "");
+
+            int voka5 = voka2.Length;
+            int voka6 = voka3 - voka5;
+
+            Console.WriteLine($"Syötteessä {voka} on {voka6} vokaalia");
+            Console.ReadLine();
+        
+        }
+    }
+}
